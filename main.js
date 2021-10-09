362c12e342a6dfa4fe603a1b8717a0565c19423f
JairoMaxter <jairomaxter@gmail.com>
2021-10-09 20:53:39 +0000

    falta

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
-function domsBuilder() {
-    var pronoun = ['the', 'our'];
-    var adj = ['great', 'big'];
-    var noun = ['jogger', 'racoon'];
-    const doms = [];
+var pronoun = ['the', 'our'];
+var adj = ['great', 'big'];
+var noun = ['jogger', 'racoon'];
+
+function allcom(pronoun, adj, noun) {
     for (let p = 0; p < pronoun.length; p++) {
         for (let a = 0; a < adj.length; a++) {
             for (let n = 0; n < noun.length; n++) {
-                const pronoun = pronoun[p];
-                const adj = adj[a];
-                const noun = noun[n]
-                
-                doms.push({ pronoun, adj, noun });
+                var domcombination = document.querySelector("#domcombination");
+                domcombination.innerHTML = `<h1 id='${p + a + n}'>www.${pronoun[p]}${adj[a]}${noun[n]}.com</h1>`;
+                console.log(`www.${pronoun[p]}${adj[a]}${noun[n]}.com`)
             }
         }
     }
-    return doms;
 }
 
-function randomCard(cards) {
-    const random = Math.floor(Math.random() * 51);
-    const cardValue = cards[random].value;
-    const cardSuit = cards[random].suit;
-    let entity;
-    cardSuit === "Diamonds" ? (entity = "&diams;") : (entity = "&" + cardSuit.toLowerCase() + ";");
-    const card = document.createElement("div");
-    card.classList.add("card", cardSuit.toLowerCase());
-    card.innerHTML =
-        '<span class="card-value-suit top">' + entity + '</span>' +
-        '<span class="card-suit">' + cardValue + '</span>' +
-        '<span class="card-value-suit bot">' + entity + '</span>';
-    document.body.appendChild(card);
-}
+allcom(pronoun, adj, noun)
